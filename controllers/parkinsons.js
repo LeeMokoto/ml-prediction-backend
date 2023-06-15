8567fa582e56c022901b601ae10e289260c016a8
Leepo.Mokoto <leepo.mokoto@disraptor.co.za>
2023-06-15 15:51:12 +0200

    add parkinsons endpoint

---
+++ controllers/parkinsons.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+
+const retry = async (func, retries = 3, delay = 1000) => {
+  try {
+    return await func();
+  } catch (err) {
+    if (retries <= 0) {
+      throw err;
+    }
+    await new Promise(resolve => setTimeout(resolve, delay));
+    return await retry(func, retries - 1, delay);
+  }
+};
+
+const predictParkinsons = async (req, res) => {
+    try {
+      const { data } = req.body;
+      
+      if (!data) {
+        return res.status(400).send("Invalid data");
+      }
+  
+      const requestToAWS = async (retryCount = 0) => {
+        try {
+          const response = await axios({
+            method: 'post',
+            url: 'https://tiin7qv5zk.execute-api.us-east-1.amazonaws.com/prod/predictparkinsons',
+            headers: {
+              'Content-type': 'application/json'
+            },
+            data:{
+              data: data
+            }
+          });
+          return response.data;
+        } catch (error) {
+          if (retryCount < 2) {
+            console.log(`Request failed, retrying in ${delay / 1000} seconds`);
+            await new Promise(resolve => setTimeout(resolve, delay));
+            return requestToAWS(retryCount + 1);
+          } else {
+            console.log(error);
+            throw new Error('Request to AWS failed after 3 attempts.');
+          }
+        }
+      };
+  
+      const respFromAWS = await retry(() => requestToAWS());
+      return res.json({ response: respFromAWS });
+  
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+module.exports = {predictParkinsons};
