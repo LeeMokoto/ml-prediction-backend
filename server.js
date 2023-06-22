cd859b98c5df48d930b186e4db9a3836d5162a33
LeeMokoto <leepo.mokoto15@gmail.com>
2023-06-22 20:02:15 +0200

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const parkinsonsRoutes = require('./routes/parkinsons'); // import the routes
 const routes = require('./routes/chestXray');
+const cors = require('cors');
 
 const app = express();
 
+//apply middleware
+app.use(cors());
 app.use(express.json({limit: "5mb"})); // parses incoming requests with JSON payloads
 
 app.use('/', parkinsonsRoutes); //to use the routes
