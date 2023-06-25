717ac0eb918359af64fcad3e006214312359fa38
Leepo.Mokoto <leepo.mokoto@disraptor.co.za>
2023-06-25 16:10:08 +0200

    add zip to workflow

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const bodyParser =  require("body-parser");
+const express = require("express");
+const parkinsonsRoutes = require('./routes/parkinsons'); // import the routes
+const routes = require('./routes/chestXray');
+const cors = require('cors');
+const serverless = require('serverless-http');
+const app = express();
 
-const app = exepress();
-const port = 8000;
+//apply middleware
+app.use(cors());
+app.use(express.json({limit: "5mb"})); // parses incoming requests with JSON payloads
 
-app.use(exepress.json());
-app.use(bodyParser.urlencoded({extended: true }));
-app.use(bodyParser.json());
+app.use('/', parkinsonsRoutes); //to use the routes
+app.use('/', routes);
 
-app.get("/", (req, res) => {
-    res.send(`<h1>Hello!</h1>`)
-});
 
-app.listen(port, () => {
-    console.log(`Application is listening at port ${port}`);
-});
+exports.handler = serverless(app);
+// const listener = app.listen(process.env.PORT || 3000, () => {
+//     console.log('App is listening on port ' + listener.address().port)
+// })
